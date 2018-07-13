95c5a15f5f4c8904a725abafac9faf04cb10bd0a
So Negishi <so-negishi@cookpad.com>
2018-07-13 10:55:34 +0900

    Add uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,27 @@
+#!/usr/local/bin/zsh
+
+# Uninstall GO
+sudo rm -rf $(which dep)
+sudo rm -rf $(which go)
+
+# Uninstall goenv
+sudo rm -rf $(which goenv)
+
+# Uninstall Rails
+sudo rm -rf $(which rails)
+
+# Uninstall Ruby
+sudo rm -rf $(which ruby)
+
+# Uninstall rbenv
+sudo rm -rf $(which rbenv)
+
+# Uninstall python2
+
+
+# Uninstall python3
+
+
+# Uninstall pyenv
+
+
