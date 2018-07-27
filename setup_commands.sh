ade476acf73e241d91f88cedb200eef889794f7a
sonegishi <5055ngs@gmail.com>
2018-07-27 12:39:37 +0900

    Modify setup_commands.sh

--- setup_commands.sh
+++ setup_commands.sh
@@ -6,41 +6,51 @@ cd
 # Install Homebrew (brew)
 echo Installing brew...
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
 
 echo Installing brew cask...
 brew tap caskroom/cask
 
-# Install rbenv using brew
-brew install rbenv ruby-build
-
-# Set rbenv PATH on zshrc
-echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.zshrc
+# Install Ruby thru rbenv using brew
+brew install rbenv
+echo rbenv --version
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
 source ~/.zshrc
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
 
 echo Installing rbenv
 rbenv install 2.5.1
 rbenv global 2.5.1
+ruby -v
 
-# Install Rails
+## Install Rails and Bundler
 echo Installing Rails
 gem install rails
+gem install bundler
 rbenv rehash
 
-# Install go using goenv
+# Install go thru goenv using brew
 brew install goenv
-
-echo 'export GOENV_ROOT=$HOME/.goenv' >> ~/.zshrc
-echo 'export PATH=$GOENV_ROOT/bin:$PATH' >> ~/.zshrc
-
-eval "$(goenv init -)"
+echo goenv --version
+echo 'eval "$(goenv init -)"' >> ~/.zshrc
+source ~/.zshrc
 
 goenv install 1.10.3
 goenv global 1.10.3
 goenv rehash
+go version
 
-echo 'export GOPATH=$HOME/go' >> ~/.zshrc
-echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.zshrc
-source ~./.zshrc
+# Install python thru pyenv using brew
+brew install pyenv
+echo pyenv --version
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+source ~/.zshrc
+
+pyenv install anaconda2-5.2.0
+pyenv install anaconda3-5.2.0
+pyenv global anaconda3-5.2.0
+pyenv rehash
+python --version
 
 # Install MySQL
 echo Installing MySQL
